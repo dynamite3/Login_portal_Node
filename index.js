424e5d0f273536bdf3e50890af6f4fcfd25f19f9
Kaustubh Magdum <37260920+dynamite3@users.noreply.github.com>
2022-01-09 10:09:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ import { auth } from "./middleware/auth.js";
 import nodemailer from "nodemailer";
 
 
-const GMAIL_PASSWORD=process.env.GMAIL_PASSWORD
-
-
 
 import localStorage from "localStorage"
 
@@ -24,6 +21,9 @@ app.use(cors());
 app.use(express.json());
 
 //loading data from hidden env
+
+// const GMAIL_PASSWORD=process.env.GMAIL_PASSWORD
+
 const PORT = process.env.PORT
 const MONGO_URL = process.env.MONGO_URL
 const SECRET_KEY=process.env.SECRET_KEY
